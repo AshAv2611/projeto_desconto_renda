c749f6ad7490649ce400e6a0f2b69f163a125bb3
Ashley Andrade <ashleyvitoriaandrade2004@gmail.com>
2026-06-18 21:43:17 -0300

    Finalizei

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// PEGANDO ELEMENTOS DO DOM
+const formDdaos = document.querySelector('#formulario')
+
+numeros[]
+//CAPTURANDO EVENTO
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const form_dados = newFormData(formDdaos)
+
+    let nome = (form_dados.get('nome'))
+    let idade = (form_dados.get('idade'))
+    let renda = (form_dados.get('renda'))
+
+})
